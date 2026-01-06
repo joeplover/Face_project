218ce13e5b2c52d4103c623512c9fc8bc65db1eb
joeplover <1665637752@qq.com>
2026-01-06 13:19:24 +0800

    项目初始化

---
+++ Smart-WMS/src/main/java/com/joe/smartwms/SmartWmsApplication.java
@@ -0,0 +1,13 @@
+package com.joe.smartwms;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SmartWmsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SmartWmsApplication.class, args);
+    }
+
+}
